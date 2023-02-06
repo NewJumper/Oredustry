1e9a91e0ea65c098eae9034e0eec1565593d61f9
muhta <muhtasim.nabi@gmail.com>
2023-02-05 21:52:59 -0600

    compressor recipes are now shown using JEI

---
+++ src/main/java/com/newjumper/oredustry/integration/CompressingCategory.java
@@ -0,0 +1,82 @@
+package com.newjumper.oredustry.integration;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.newjumper.oredustry.Oredustry;
+import com.newjumper.oredustry.block.OredustryBlocks;
+import com.newjumper.oredustry.recipe.CompressingRecipe;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+@SuppressWarnings("NullableProblems")
+public class CompressingCategory implements IRecipeCategory<CompressingRecipe> {
+    public final static ResourceLocation TEXTURE = new ResourceLocation(Oredustry.MOD_ID, "textures/gui/container/compressor.png");
+
+    private final IDrawable background;
+    private final IDrawable icon;
+    private final LoadingCache<Integer, IDrawableAnimated> progress;
+    private final int time;
+
+    public CompressingCategory(IGuiHelper guiHelper, int time) {
+        this.background = guiHelper.createDrawable(TEXTURE, 35, 18, 106, 50);
+        this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(OredustryBlocks.COMPRESSOR.get()));
+
+        this.progress = CacheBuilder.newBuilder().maximumSize(21).build(new CacheLoader<>() {
+            @Override
+            public IDrawableAnimated load(Integer time) {
+                return guiHelper.drawableBuilder(TEXTURE, 176, 36, 20, 13).buildAnimated(time, IDrawableAnimated.StartDirection.LEFT, false);
+            }
+        });
+        this.time = time;
+    }
+
+    @Override
+    public RecipeType<CompressingRecipe> getRecipeType() {
+        return RecipeType.create(Oredustry.MOD_ID, "compressing", CompressingRecipe.class);
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("container." + Oredustry.MOD_ID + "." + OredustryBlocks.COMPRESSOR.getId().getPath());
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, CompressingRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 29, 17).addIngredients(recipe.getIngredient());
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 83, 17).addItemStack(recipe.getResultItem());
+    }
+
+    @Override
+    public void draw(CompressingRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+        this.progress.getUnchecked(time / 2).draw(stack, 51, 19);
+
+        Font fontRenderer = Minecraft.getInstance().font;
+        int stringWidth = fontRenderer.width(time / 20 + "s");
+        fontRenderer.draw(stack, time / 20 + "s", background.getWidth() - stringWidth, background.getHeight() - 7, 0xff808080);
+    }
+}
--- src/main/java/com/newjumper/oredustry/integration/OredustryJEI.java
+++ src/main/java/com/newjumper/oredustry/integration/OredustryJEI.java
@@ -2,6 +2,7 @@ package com.newjumper.oredustry.integration;
 
 import com.newjumper.oredustry.Oredustry;
 import com.newjumper.oredustry.block.OredustryBlocks;
+import com.newjumper.oredustry.recipe.CompressingRecipe;
 import com.newjumper.oredustry.recipe.MeltingRecipe;
 import com.newjumper.oredustry.recipe.SeparatingRecipe;
 import mezz.jei.api.IModPlugin;
@@ -31,22 +32,25 @@ public class OredustryJEI implements IModPlugin {
     public void registerCategories(IRecipeCategoryRegistration registration) {
         IGuiHelper guiHelper = registration.getJeiHelpers().getGuiHelper();
 
-        registration.addRecipeCategories(new SeparatingCategory(guiHelper, 200), new MeltingCategory(guiHelper, 150));
+        registration.addRecipeCategories(new CompressingCategory(guiHelper, 200), new MeltingCategory(guiHelper, 150), new SeparatingCategory(guiHelper, 200));
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
         RecipeManager recipes = Objects.requireNonNull(Minecraft.getInstance().level).getRecipeManager();
-        List<SeparatingRecipe> separating = recipes.getAllRecipesFor(SeparatingRecipe.Type.INSTANCE);
+        List<CompressingRecipe> compressing = recipes.getAllRecipesFor(CompressingRecipe.Type.INSTANCE);
         List<MeltingRecipe> melting = recipes.getAllRecipesFor(MeltingRecipe.Type.INSTANCE);
+        List<SeparatingRecipe> separating = recipes.getAllRecipesFor(SeparatingRecipe.Type.INSTANCE);
 
-        registration.addRecipes(RecipeType.create(Oredustry.MOD_ID, "separating", SeparatingRecipe.class), separating);
+        registration.addRecipes(RecipeType.create(Oredustry.MOD_ID, "compressing", CompressingRecipe.class), compressing);
         registration.addRecipes(RecipeType.create(Oredustry.MOD_ID, "melting", MeltingRecipe.class), melting);
+        registration.addRecipes(RecipeType.create(Oredustry.MOD_ID, "separating", SeparatingRecipe.class), separating);
     }
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
-        registration.addRecipeCatalyst(new ItemStack(OredustryBlocks.SEPARATOR.get()), RecipeType.create(Oredustry.MOD_ID, "separating", SeparatingRecipe.class));
+        registration.addRecipeCatalyst(new ItemStack(OredustryBlocks.COMPRESSOR.get()), RecipeType.create(Oredustry.MOD_ID, "compressing", CompressingRecipe.class));
         registration.addRecipeCatalyst(new ItemStack(OredustryBlocks.CRUCIBLE.get()), RecipeType.create(Oredustry.MOD_ID, "melting", MeltingRecipe.class));
+        registration.addRecipeCatalyst(new ItemStack(OredustryBlocks.SEPARATOR.get()), RecipeType.create(Oredustry.MOD_ID, "separating", SeparatingRecipe.class));
     }
 }
