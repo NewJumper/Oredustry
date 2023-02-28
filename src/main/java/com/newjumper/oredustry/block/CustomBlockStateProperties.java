48842309d51746eb0c0e1f010000dafa1fd7febb
muhta <muhtasim.nabi@gmail.com>
2023-02-27 22:32:42 -0600

    deleted CustomBlockStateProperties.java

--- src/main/java/com/newjumper/oredustry/block/CustomBlockStateProperties.java
+++
@@ -1,7 +0,0 @@
-package com.newjumper.oredustry.block;
-
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-
-public class CustomBlockStateProperties {
-    public static final BooleanProperty ACTIVE = BooleanProperty.create("active");
-}
