b94b4ab21de404820318335623b8b7993369f3e5
muhta <muhtasim.nabi@gmail.com>
2023-05-29 11:49:43 -0500

    upgrades' GUI now opens when the wrench is clicked, updated its GUI

--- src/main/java/com/newjumper/oredustry/screen/SeparatorScreen.java
+++ src/main/java/com/newjumper/oredustry/screen/SeparatorScreen.java
@@ -3,16 +3,21 @@ package com.newjumper.oredustry.screen;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.newjumper.oredustry.Oredustry;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.player.Inventory;
 
 @SuppressWarnings("NullableProblems")
 public class SeparatorScreen extends AbstractContainerScreen<SeparatorMenu> {
     public static final ResourceLocation GUI = new ResourceLocation(Oredustry.MOD_ID, "textures/gui/container/separator.png");
     public static final ResourceLocation UPGRADES = new ResourceLocation(Oredustry.MOD_ID, "textures/gui/upgrades.png");
+
+    private boolean upgradesGUI;
 
     public SeparatorScreen(SeparatorMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
@@ -44,6 +49,17 @@ public class SeparatorScreen extends AbstractContainerScreen<SeparatorMenu> {
         if(menu.drawProgress() > 0) this.blit(pPoseStack, x + 82, y + 33, 176, 14, menu.drawProgress(), 21);
 
         RenderSystem.setShaderTexture(0, UPGRADES);
-        this.blit(pPoseStack, x + imageWidth - 3, y + imageHeight - 32, 86, 0, 23, 26);
+        if(upgradesGUI) this.blit(pPoseStack, x + imageWidth - 3, y, 0, 0, 64, 86);
+        else this.blit(pPoseStack, x + imageWidth - 3, y, 64, 0, 23, 26);
+    }
+
+    @Override
+    public boolean mouseClicked(double pMouseX, double pMouseY, int pButton) {
+        if(isHovering(imageWidth - 1, 5, 16, 16, pMouseX, pMouseY)) {
+            Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 0.6f, 0.3f));
+            upgradesGUI = !upgradesGUI;
+        }
+
+        return super.mouseClicked(pMouseX, pMouseY, pButton);
     }
 }
